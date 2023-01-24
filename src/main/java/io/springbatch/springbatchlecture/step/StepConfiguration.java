7af9856601f52742923c4aa9e79955b4eabb3b71
Jihun Park <jihunpark.tech@gmail.com>
2023-01-24 21:30:58 +0900

    [Spring Batch] Step

---
+++ src/main/java/io/springbatch/springbatchlecture/step/StepConfiguration.java
@@ -0,0 +1,54 @@
+package io.springbatch.springbatchlecture.step;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class StepConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job BatchJob() {
+        return this.jobBuilderFactory.get("StepExample")
+                .start(StepExampleStep1())
+                .next(StepExampleStep2())
+                .build();
+    }
+
+    @Bean
+    public Step StepExampleStep1() {
+        return stepBuilderFactory.get("StepExampleStep1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        log.info("step1 has executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Step StepExampleStep2() {
+        return stepBuilderFactory.get("StepExampleStep2")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("step2 has executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
