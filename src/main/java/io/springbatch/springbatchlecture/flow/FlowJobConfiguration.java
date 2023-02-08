a39a5882e236b57af5d5e279081b7d062c54a4d0
Jihun Park <jihunpark.tech@gmail.com>
2023-02-08 20:54:29 +0900

    [Spring Batch Step] FlowJob 개념 및 API 소개

---
+++ src/main/java/io/springbatch/springbatchlecture/flow/FlowJobConfiguration.java
@@ -0,0 +1,68 @@
+package io.springbatch.springbatchlecture.flow;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class FlowJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job FlowJobConfiguration() {
+        return jobBuilderFactory.get("FlowJobConfiguration")
+                .start(FlowJobConfigurationStep1())
+                .on("COMPLETED").to(FlowJobConfigurationStep2())
+                .from(FlowJobConfigurationStep1())
+                .on("FAILED").to(FlowJobConfigurationStep3())
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step FlowJobConfigurationStep1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step1 has executed");
+//                        throw new RuntimeException("");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+    }
+
+    @Bean
+    public Step FlowJobConfigurationStep2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step2 has executed");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Step FlowJobConfigurationStep3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step3 has executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+    }
+
+
+}
