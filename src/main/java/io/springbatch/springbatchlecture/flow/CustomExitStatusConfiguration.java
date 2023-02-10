c537b6e771f772a2e9a927bfe089561250c8471d
Jihun Park <jihunpark.tech@gmail.com>
2023-02-10 10:06:28 +0900

    [Spring Batch Flow] 사용자 정의 ExitStatus

---
+++ src/main/java/io/springbatch/springbatchlecture/flow/CustomExitStatusConfiguration.java
@@ -0,0 +1,68 @@
+package io.springbatch.springbatchlecture.flow;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.listener.StepExecutionListenerSupport;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class CustomExitStatusConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job CustomExitStatusConfiguration() {
+        return this.jobBuilderFactory.get("CustomExitStatusConfiguration")
+                .start(step1())
+                    .on("FAILED")
+                    .to(step2())
+                    .on("PASS")
+                    .stop()
+                .end()
+                .build();
+    }
+
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">> step1 has executed");
+                    contribution.setExitStatus(ExitStatus.FAILED);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">> step2 has executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .listener(new PassCheckingListener())
+                .build();
+    }
+
+    static class PassCheckingListener extends StepExecutionListenerSupport {
+
+        @Override
+        public ExitStatus afterStep(StepExecution stepExecution) {
+
+            String exitCode = stepExecution.getExitStatus().getExitCode();
+            if (!exitCode.equals(ExitStatus.FAILED.getExitCode())) {
+                return new ExitStatus("PASS");
+            } else {
+                return null;
+            }
+        }
+    }
+
+}
